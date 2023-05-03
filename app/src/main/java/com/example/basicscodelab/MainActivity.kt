94202a1562db4cd887f577061337c05980ead71a
aldian <yohputra02@gmail.com>
2023-05-03 13:28:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.basicscodelab
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                    myApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun myApp(){
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }else{
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(100) {"$it"}
+) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        LazyColumn {
+            items(names){ name->
+             Greeting(name = name)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Composable
+private fun Greeting(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        targetValue = if(expanded) 48.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 2000
+        )
+    )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = {expanded = !expanded}){
+                Text(if (expanded)"Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked:()-> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick =  onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
